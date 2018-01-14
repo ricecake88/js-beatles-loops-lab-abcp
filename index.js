816bd127890a9ed2ce3319b6e8f29d167de9ee34
Grace Shih <vocalise@gmail.com>
2018-01-14 17:07:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicianArray, instrumentArray) {
   var newArray = [];
   
   for (var i=0; i < musicianArray.length; i++) {
-    if (i === 0) newArray[i] = `${musicianArray[i]} plays ${instrumentArray[i]}`;
+    newArray[i] = `${musicianArray[i]} plays ${instrumentArray[i]}`;
   }
   
   return newArray;
